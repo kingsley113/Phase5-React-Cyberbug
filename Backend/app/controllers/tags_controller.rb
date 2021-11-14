b645b2b300a25cd7a66b3f63ad69b0f9bf9ebd01
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-13 16:44:20 -0800

    define tag controller methods

--- Backend/app/controllers/tags_controller.rb
+++ Backend/app/controllers/tags_controller.rb
@@ -1,12 +1,39 @@
 class TagsController < ApplicationController
 	# Create
+	def new
+		tag = Tag.new(tag_params)
+
+		if tag.save?
+			render json: tag
+			# TODO:serializer?
+		else
+			render text: "Unable to create tag", status: :unprocessable_entity
+		end
+	end 
+
 	# Read
+
+	def all 
+		tags = Tag.all
+		render json: tags
+	end
+	
+	def show
+		tag = Tag.find_by(id: tag_params[:id])
+
+		if tag
+			render json: tag
+		else
+			render text: "Tag not found"
+		end
+	end
+
 	# Update?
 	# Destroy?
 
 	private
 
 	def tag_params(params)
-		params.require(:tag).permit(:name)
+		params.require(:tag).permit(:id, :name)
 	end
 end
