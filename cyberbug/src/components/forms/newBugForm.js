36c62e947e8ebb9a91cfef23eb3541aefc54de7d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-15 10:37:46 -0800

    Create form component outlines

---
+++ cyberbug/src/components/forms/newBugForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NewBugForm extends Component {
+  render() {
+    return (
+      <div>
+        This is the new bug form :)
+        {/* TODO: fill in an create form content */}
+      </div>
+    );
+  }
+}
+
+export default NewBugForm;
---
+++ cyberbug/src/components/forms/newProjectForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NewProjectForm extends Component {
+  render() {
+    return (
+      <div>
+        This is the new project form :)
+        {/* TODO: create this form content */}
+      </div>
+    );
+  }
+}
+
+export default NewProjectForm;
