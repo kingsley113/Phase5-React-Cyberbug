1f2bff02ef7e8a5266b7352a258f8022db044f57
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-15 10:44:10 -0800

    rough out Modal window components

---
+++ cyberbug/src/containers/modalWindow.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ModalWindow extends Component {
+  render() {
+    return (
+      <div>
+        -This is the modal window component :)
+        {/* TODO: create modal window and integrate using multiple different components, pass in the component to show as a prop? */}
+      </div>
+    );
+  }
+}
+
+export default ModalWindow;
