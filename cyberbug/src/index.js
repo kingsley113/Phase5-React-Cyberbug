7ce7aa02f70362a91e3220ade64244671c475bd3
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-16 22:41:25 -0800

    Set up React Router

--- cyberbug/src/index.js
+++ cyberbug/src/index.js
@@ -3,19 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { createStore } from "redux";
 import rootReducer from "./reducers/index";
 import { Provider } from "react-redux";
+
+import SplashScreen from "./components/splashScreen";
 
 let store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+      <Route exact path="/login" component={SplashScreen} />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
