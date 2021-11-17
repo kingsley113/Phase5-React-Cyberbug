3575008ab3e0964d0d5b870446b63d377677db6a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-16 16:14:15 -0800

    Create dummy project dropdown

--- cyberbug/src/components/pageHeader/activeProjectSelector.js
+++ cyberbug/src/components/pageHeader/activeProjectSelector.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
 class ActiveProjectSelector extends Component {
+  handleOnChange = (event) => {
+    console.log("This would switch to project: ", event.target.value);
+  };
+
   render() {
     return (
       <div>
-        -This is the active project dropdown component :)
         {/* TODO: fill in selector content */}
+        <form>
+          <select
+            name="active-project"
+            id="active-project-dropdown"
+            onChange={this.handleOnChange}
+          >
+            <option value="dummy-project-1">Dummy Project 1</option>
+            <option value="dummy-project-2">Dummy Project 2</option>
+            <option value="dummy-project-3">Dummy Project 3</option>
+            <option value="dummy-project-4">Dummy Project 4</option>
+          </select>
+        </form>
       </div>
     );
   }
