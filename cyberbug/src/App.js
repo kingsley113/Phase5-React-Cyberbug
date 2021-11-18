77899486c2b4e44bca364a1f3fbd84edd401c075
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-18 12:42:49 -0800

    Relocate Route wrapper to App component and clean up

--- cyberbug/src/App.js
+++ cyberbug/src/App.js
@@ -1,46 +1,27 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import PageHeader from "./components/pageHeader";
 import PageContent from "./containers/pageContent";
 import PageSidebar from "./components/pageSidebar";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends Component {
   render() {
     return (
-      <div>
+      // <div>
+      <Router>
         <div>
           <PageHeader />
         </div>
         <div className="page-main">
           {/* Sidebar */}
-          {/* <PageSidebar /> */}
+          <PageSidebar />
           {/* Content */}
           <PageContent />
         </div>
-      </div>
+        {/* </div> */}
+      </Router>
       // TODO: Add logic to determine if user is logged in or not and show splash screen if not
     );
   }
--- cyberbug/src/containers/pageContent.js
+++ cyberbug/src/containers/pageContent.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-// import PageHeader from "../components/pageHeader";
-// import PageSidebar from "../components/pageSidebar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import PageSidebar from "../components/pageSidebar";
+import { Route } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard";
 import SplashScreen from "../components/splashScreen";
@@ -14,19 +10,17 @@ import ProjectsPage from "./pages/projectsPage";
 class PageContent extends Component {
   render() {
     return (
-      <Router>
-        <PageSidebar />
+      <div>
         <Route exact path="/" component={Dashboard} />
         <Route exact path="/login" component={SplashScreen} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/projects" component={ProjectsPage} />
         <Route exact path="/projects/:id" component={ProjectPage} />
         <Route exact path="/settings" component={SettingsPage} />
-      </Router>
+      </div>
       /* Modal window for forms */
       /* <ModalWindow /> */
       /* Only need modal window component for forms, should this be within form components? TODO: */
-      /* Header */
     );
   }
 }
