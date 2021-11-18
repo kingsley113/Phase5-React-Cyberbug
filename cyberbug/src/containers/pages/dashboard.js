414f4e50ec0b5118741ae9ba2c3d4d8a10974c23
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-18 12:51:33 -0800

    Reorganize ProjectStatus component

--- cyberbug/src/containers/pages/dashboard.js
+++ cyberbug/src/containers/pages/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ProjectStatus from "../../components/dashboard/projectStatus";
+// import ProjectStatus from "../../components/project/projectStatus";
 import ProjectsChart from "../../components/dashboard/projectsChart";
 import PriorityTasks from "../../components/dashboard/priorityTasks";
 import DailyInfoPanel from "../../components/dashboard/dailyInfoPanel";
@@ -12,10 +12,10 @@ class Dashboard extends Component {
       <div>
         -This is the dashboard component :)
         {/* TODO: Fill in the dashboard content here */}
-        <ProjectStatus />
-        <ProjectsChart />
         <PriorityTasks />
+        <ProjectsChart />
         <DailyInfoPanel />
+        {/* <ProjectStatus /> */}
         <ProjectsList />
       </div>
     );
--- cyberbug/src/containers/pages/projectPage.js
+++ cyberbug/src/containers/pages/projectPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ProjectSummary from "../../components/project/projectSummary";
 import BugList from "../../components/project/bugList";
 import BugDetails from "../../components/project/bugDetails";
+import ProjectStatus from "../../components/project/projectStatus";
 
 class ProjectPage extends Component {
   render() {
@@ -10,6 +11,7 @@ class ProjectPage extends Component {
         -This is the project page component :)
         {/* TODO: Fill in the project page content */}
         <ProjectSummary />
+        <ProjectStatus />
         <BugList />
         <BugDetails />
       </div>
