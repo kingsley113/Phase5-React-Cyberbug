073ebfe4b496f260a732d2cb8ec280bc6a2a7b87
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-28 02:10:24 -0800

    Implement random quote component in header

--- cyberbug_frontend/src/components/pageHeader.js
+++ cyberbug_frontend/src/components/pageHeader.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ActiveProjectSelector from "./pageHeader/activeProjectSelector";
+// import ActiveProjectSelector from "./pageHeader/activeProjectSelector";
+import RandomQuote from "./pageHeader/randomQuote";
 import CurrentUserPanel from "./pageHeader/currentUserPanel";
 import { connect } from "react-redux";
 
@@ -9,7 +10,8 @@ class PageHeader extends Component {
       <div className="page-header">
         {/* TODO: Add header content here */}
         <img src="cyberbugLogoQuick.png" alt="Cyberbug Logo" className="logo" />
-        <ActiveProjectSelector projects={this.props.projects} />
+        {/* <ActiveProjectSelector projects={this.props.projects} /> */}
+        <RandomQuote />
         <CurrentUserPanel />
       </div>
     );
