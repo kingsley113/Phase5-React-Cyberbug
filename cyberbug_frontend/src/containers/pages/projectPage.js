ed813053bb4d857f428ee05f42ba218e9cd78eee
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-28 19:01:50 -0800

    clean up code for displaying selected project

--- cyberbug_frontend/src/containers/pages/projectPage.js
+++ cyberbug_frontend/src/containers/pages/projectPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 
 import { setActiveProject } from "../../actions/projectActions";
@@ -8,53 +8,38 @@ import BugList from "../../components/project/bugList";
 import BugDetails from "../../components/project/bugDetails";
 import ProjectStatus from "../../components/project/projectStatus";
 
-class ProjectPage extends Component {
-  state = {
-    project: this.setProject(),
-  };
-
-  setProject() {
+class ProjectPage extends PureComponent {
+  filterProject() {
     if (this.props.projects) {
-      return this.props.projects.filter(
+      const project = this.props.projects.filter(
         (project) => project.projectId === this.props.match.params.id
       )[0];
+      return project;
     }
   }
 
-  renderProjectInfo(project = null) {
-    if (project) {
-      // const project = this.state.project;
-      return (
-        <div>
-          <h3>{project.projectTitle}</h3>
-          <p>{project.projectId}</p>
-        </div>
-      );
-    }
-  }
+  // renderProjectInfo() {
+  //   const project = this.filterProject();
+  //   return (
+  //     <div>
+  //       <h3>{project.projectTitle}</h3>
+  //       <p>{project.projectId}</p>
+  //     </div>
+  //   );
+  // }
 
   render() {
-    const project = this.setProject();
-    // this.setState({
-    //   project: this.setProject(),
-    // });
-    // console.log(this.props.match.params.id);
-    console.log(this.state.project);
     return (
       <div>
         -This is the project page component :)
         {/* TODO: Fill in the project page content */}
-        {this.renderProjectInfo(project)}
-        <ProjectSummary />
+        {/* {this.renderProjectInfo()} */}
+        <ProjectSummary project={this.filterProject()} />
         <ProjectStatus />
         <BugList />
         <BugDetails />
       </div>
     );
-  }
-
-  componentDidMount() {
-    // this.props.setActiveProject(this.props.match.params.id);
   }
 }
 
