d7cd4dada0c97ab7419790baae4a4d4eecfc11ed
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-30 19:16:16 -0800

    update conditional returns to handle loading

--- cyberbug_frontend/src/components/project/bugList.js
+++ cyberbug_frontend/src/components/project/bugList.js
@@ -3,31 +3,37 @@ import { connect } from "react-redux";
 import BugLineItem from "./bugLineItem";
 
 class BugList extends Component {
-  constructor(props) {
-    super();
-    this.project = props.project;
-    this.bugs = props.bugs;
-    this.route = props.route;
-  }
+  // constructor(props) {
+  //   super();
+  //   this.project = props.project;
+  //   this.bugs = props.bugs;
+  //   this.route = props.route;
+  // }
 
   renderBugs = () => {
-    if (this.bugs.allBugs) {
-      const bugs = this.bugs.allBugs.filter(
-        (bug) => bug.project_id === this.project.id
+    if (this.props.bugs.allBugs) {
+      const bugs = this.props.bugs.allBugs.filter(
+        (bug) => bug.project_id === this.props.project.id
       );
 
       const bugList = bugs.map((bug, id) => {
-        return <BugLineItem key={id} bug={bug} url={this.route} />;
+        return <BugLineItem key={id} bug={bug} url={this.props.route} />;
       });
       if (bugList.length > 0) {
         return bugList;
       } else {
-        // return <h3>No bugs! :)</h3>;
-        return null;
+        return (
+          <tr>
+            <td>No Bugs!</td>
+          </tr>
+        );
       }
     } else {
-      // return <h3>Loading Bugs...</h3>;
-      return null;
+      return (
+        <tr>
+          <td>Loading</td>
+        </tr>
+      );
     }
   };
 
@@ -49,7 +55,7 @@ class BugList extends Component {
         </table>
         --This is the bug list component :)
         {/* TODO: fill in bug list content */}
-        <ul>{this.renderBugs()}</ul>
+        {/* <ul>{this.renderBugs()}</ul> */}
       </div>
     );
   }
