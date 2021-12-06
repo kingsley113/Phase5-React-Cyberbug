763235ccb0c297821ee5e039e723d7781a138f7f
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-06 15:08:58 -0800

    add JWT encode and decode methods

--- cyberbug_backend/app/controllers/application_controller.rb
+++ cyberbug_backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+	def encode_token(payload)
+		JWT.encode(payload, "squish_all_of_the_bugs!")
+	end
+
+	def decode_token(token)
+		JWT.decode(token, "squish_all_of_the_bugs!")[0]
+	end
 end
