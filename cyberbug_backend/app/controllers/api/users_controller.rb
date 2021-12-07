bc6dd2eeb624e59f4332737826562769b82997c5
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-06 16:50:55 -0800

    modify users controller to issue token on create

--- cyberbug_backend/app/controllers/api/users_controller.rb
+++ cyberbug_backend/app/controllers/api/users_controller.rb
@@ -1,10 +1,13 @@
 class Api::UsersController < ApplicationController
+	skip_before_action :authorized, only: [:create]
+	
 	# Create
 	def create
 		@user = User.create(user_params)
 
 		if @user.valid?
-			render json: { user: UserSerializer.new(@user) }, status: :created
+			@token = encode_token(user_id: @user.id)
+			render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
 		else
 			render json: {error: 'error creating user' }, status: :unprocessable_entity
 		end
