b7ffd3c7144a206275135c77408201ce6621f51d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-06 20:08:19 -0800

    clean up login page unused code

--- cyberbug_frontend/src/containers/pages/loginPage.js
+++ cyberbug_frontend/src/containers/pages/loginPage.js
@@ -1,49 +1,12 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router";
 import { Link } from "react-router-dom";
 import LoginForm from "../../components/forms/loginForm";
-// import PageSidebar from "./pageSidebar";
 
 class LoginPage extends Component {
-  // state = {
-  //   username: "",
-  //   password: "",
-  // };
-
-  // handleOnSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log("Inside handleOnSubmit function, Form would be submitted");
-  // };
-
-  // handleOnChange = (event) => {
-  //   this.setState({
-  //     [event.target.name]: event.target.value,
-  //   });
-  // };
-
   render() {
     return (
       <div>
         <LoginForm />
-        {/* <form id="login-form" onSubmit={this.handleOnSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            onChange={this.handleOnChange}
-            value={this.state.username}
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            onChange={this.handleOnChange}
-            value={this.state.password}
-          />
-          <input type="submit" />
-        </form> */}
         <Link to="/signup">
           <button id="create-account">Create Account</button>
         </Link>
@@ -54,3 +17,4 @@ class LoginPage extends Component {
 
 export default LoginPage;
 // TODO: Add styling
+// TODO: Could this be a simple component rather than a class?
