fc19df3528700721f56d8247b00d767efc3fbd00
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-06 17:00:46 -0800

    create auth_controller

--- cyberbug_backend/app/controllers/api/users_controller.rb
+++ cyberbug_backend/app/controllers/api/users_controller.rb
@@ -6,7 +6,7 @@ class Api::UsersController < ApplicationController
 		@user = User.create(user_params)
 
 		if @user.valid?
-			@token = encode_token(user_id: @user.id)
+			@token = encode_token({user_id: @user.id})
 			render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
 		else
 			render json: {error: 'error creating user' }, status: :unprocessable_entity
