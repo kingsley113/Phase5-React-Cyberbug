2cf07069b407fba4008da5dc880ca201191afa20
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-06 19:03:39 -0800

    Refactor component organization to allow login page

--- cyberbug_frontend/src/App.js
+++ cyberbug_frontend/src/App.js
@@ -1,45 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { connect } from "react-redux";
-import { loadProjects } from "./actions/projectActions";
-import { loadBugs } from "./actions/bugActions";
-
-import PageHeader from "./components/pageHeader";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PageContent from "./containers/pageContent";
-import PageSidebar from "./components/pageSidebar";
+import SplashScreen from "./components/splashScreen";
 
 class App extends Component {
   render() {
     return (
       // <div>
       <Router>
-        <div>
-          <PageHeader />
-        </div>
-        <div className="page-main">
-          {/* Sidebar */}
-          <PageSidebar />
-          {/* Content */}
-          <PageContent />
-        </div>
-        {/* </div> */}
+        <Switch>
+          <Route exact path="/login" component={SplashScreen} />
+          {/* TODO: Rename splash screen to login screen */}
+          <Route path="/" component={PageContent} />
+          {/* TODO: if localstorage JWT present, redirect to dashbaord, if not, redirect to login */}
+          <Route path="*">
+            {/* TODO: Not found component/page */}
+            <h2>Page Not Found</h2>
+          </Route>
+        </Switch>
       </Router>
       // TODO: Add logic to determine if user is logged in or not and show splash screen if not
     );
   }
-
-  componentDidMount() {
-    this.props.loadProjects();
-    this.props.loadBugs();
-  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    loadProjects: () => dispatch(loadProjects()),
-    loadBugs: () => dispatch(loadBugs()),
-  };
-};
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
--- cyberbug_frontend/src/containers/pageContent.js
+++ cyberbug_frontend/src/containers/pageContent.js
@@ -3,40 +3,53 @@ import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Dashboard from "./pages/dashboard";
-import SplashScreen from "../components/splashScreen";
 import ProjectPage from "./pages/projectPage";
 import SettingsPage from "./pages/settingsPage";
 import ProjectsPage from "./pages/projectsPage";
 import NewProjectForm from "../components/forms/newProjectForm";
 import ModalWindow from "./modalWindow";
 import Toggle from "./toggle";
+import PageHeader from "../components/pageHeader";
+import PageSidebar from "../components/pageSidebar";
+import { loadProjects } from "../actions/projectActions";
+import { loadBugs } from "../actions/bugActions";
 
 class PageContent extends Component {
   render() {
     return (
-      <div className="full-width">
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/login" component={SplashScreen} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/projects" component={ProjectsPage} />
-        <Route
-          // exact
-          path="/projects/:id"
-          render={(routerProps) => (
-            <ProjectPage
-              {...routerProps}
-              projects={this.props.projects}
-              activeProject={this.props.activeProject}
+      <div>
+        <div>
+          <PageHeader />
+        </div>
+        <div className="page-main">
+          <PageSidebar />
+          <div className="full-width">
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/projects" component={ProjectsPage} />
+            <Route
+              path="/projects/:id"
+              render={(routerProps) => (
+                <ProjectPage
+                  {...routerProps}
+                  projects={this.props.projects}
+                  activeProject={this.props.activeProject}
+                />
+              )}
             />
-          )}
-        />
-        <Route exact path="/settings" component={SettingsPage} />
+            <Route exact path="/settings" component={SettingsPage} />
 
-        <Toggle id={"newProjectFormToggle"}>
-          <ModalWindow component={<NewProjectForm />} />
-        </Toggle>
+            <Toggle id={"newProjectFormToggle"}>
+              <ModalWindow component={<NewProjectForm />} />
+            </Toggle>
+          </div>
+        </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.props.loadProjects();
+    this.props.loadBugs();
   }
 }
 
@@ -47,4 +60,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(PageContent);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadProjects: () => dispatch(loadProjects()),
+    loadBugs: () => dispatch(loadBugs()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageContent);
