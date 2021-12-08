0212fe29ec2495c1fc5267edf9be8194fd2155ac
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-08 00:41:08 -0800

    add project stat functions and helper methods

--- cyberbug_frontend/src/components/project/projectSummary.js
+++ cyberbug_frontend/src/components/project/projectSummary.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  overdueBugCount,
+  openBugCount,
+  closedBugCount,
+  totalBugCount,
+} from "../../helpers/projectHelpers";
 
 class ProjectSummary extends Component {
   renderProjectDetails() {
     if (this.props.project) {
       return (
         <div className="centered">
-          {/* TODO: fill in project summary content */}
+          {/* TODO: make this a table or some better way to present it... */}
           <h3>Open | Overdue | Closed | Total</h3>
-          <p>13---------6---------35-------999</p>
+          <p>
+            {openBugCount(this.props.project, this.props.bugs)}---------
+            {overdueBugCount(this.props.project, this.props.bugs)}
+            ---------{closedBugCount(this.props.project, this.props.bugs)}
+            -------{totalBugCount(this.props.project, this.props.bugs)}
+          </p>
         </div>
       );
     } else {
@@ -20,4 +32,11 @@ class ProjectSummary extends Component {
   }
 }
 
-export default ProjectSummary;
+const mapStateToProps = (state) => {
+  return {
+    bugs: state.bugs.allBugs,
+    project: state.projects.activeProject,
+  };
+};
+
+export default connect(mapStateToProps)(ProjectSummary);
--- cyberbug_frontend/src/helpers/projectHelpers.js
+++ cyberbug_frontend/src/helpers/projectHelpers.js
@@ -1,3 +1,4 @@
+// Open Bugs
 export const openBugCount = (project, bugs) => {
   let bugCount = 0;
   if (bugs) {
@@ -10,12 +11,46 @@ export const openBugCount = (project, bugs) => {
   return bugCount;
 };
 
+// Overdue Bugs
 export const overdueBugCount = (project, bugs) => {
   let bugCount = 0;
   const date = new Date();
   if (bugs) {
     for (const bug of bugs) {
-      if (bug.bugDueDate < date && !bug.bugComplete) {
+      if (bug.bugDueDate) {
+        const dueDate = new Date(bug.bugDueDate);
+        if (
+          bug.project_id === project.id &&
+          dueDate < date &&
+          bug.bugComplete === null
+        ) {
+          bugCount += 1;
+        }
+      }
+    }
+  }
+  return bugCount;
+};
+
+// Closed Bugs
+export const closedBugCount = (project, bugs) => {
+  let bugCount = 0;
+  if (bugs) {
+    for (const bug of bugs) {
+      if (bug.project_id === project.id && bug.bugComplete) {
+        bugCount += 1;
+      }
+    }
+  }
+  return bugCount;
+};
+
+// Total Bugs
+export const totalBugCount = (project, bugs) => {
+  let bugCount = 0;
+  if (bugs) {
+    for (const bug of bugs) {
+      if (bug.project_id === project.id) {
         bugCount += 1;
       }
     }
