3fe166b7ebaa91d367c5291bcd4e5cba21bade15
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-08 14:22:53 -0800

    update dashboard styling & layout

--- cyberbug_frontend/src/components/dashboard/priorityTasks.js
+++ cyberbug_frontend/src/components/dashboard/priorityTasks.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class PriorityTasks extends Component {
   render() {
     return (
-      <div className="test-border">
+      <div className="test-border" id="priority-tasks-panel">
         --This is the priority tasks component :)
         {/* TODO: fill in content */}
       </div>
--- cyberbug_frontend/src/containers/pages/dashboard.js
+++ cyberbug_frontend/src/containers/pages/dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ProjectsChart from "../../components/dashboard/projectsChart";
 import PriorityTasks from "../../components/dashboard/priorityTasks";
-import DailyInfoPanel from "../../components/dashboard/dailyInfoPanel";
+// import DailyInfoPanel from "../../components/dashboard/dailyInfoPanel";
 import ProjectsListDetailed from "../../components/dashboard/projectListDetailed";
 
 class Dashboard extends Component {
@@ -11,7 +11,7 @@ class Dashboard extends Component {
         <div className="test-border-blue dashboard-stats-container">
           <PriorityTasks />
           <ProjectsChart />
-          <DailyInfoPanel />
+          {/* <DailyInfoPanel /> */}
         </div>
         <ProjectsListDetailed />
       </div>
