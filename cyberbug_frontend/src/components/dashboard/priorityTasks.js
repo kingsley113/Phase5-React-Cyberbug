35cf8c059145e30ac3e33ab716f0e2842589a3c3
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-08 15:59:02 -0800

    Develop top 10 priority task panel

--- cyberbug_frontend/src/components/dashboard/priorityTasks.js
+++ cyberbug_frontend/src/components/dashboard/priorityTasks.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class PriorityTasks extends Component {
   render() {
@@ -6,8 +7,105 @@ class PriorityTasks extends Component {
       <div className="test-border" id="priority-tasks-panel">
         --This is the priority tasks component :)
         {/* TODO: fill in content */}
+        <table>
+          <thead>
+            <tr>
+              <th>Bug Id</th>
+              <th>Title</th>
+              <th>Priority</th>
+              <th>Due Date</th>
+              <th>Status</th>
+              <th>Project</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderPriorityTableRows()}</tbody>
+        </table>
       </div>
     );
+  }
+
+  renderPriorityTableRows() {
+    if (this.props.bugs) {
+      let bugArray = [];
+
+      // Roadblock
+      let roadblockBugs = this.props.bugs.filter(
+        (bug) =>
+          bug.bugPriority === "Roadblock" && bug.bugDueDate && !bug.bugComplete
+      );
+      roadblockBugs.sort((firstBug, secondBug) => {
+        return new Date(firstBug.bugDueDate) - new Date(secondBug.bugDueDate);
+      });
+      bugArray = bugArray.concat(roadblockBugs);
+
+      // Critical
+      let criticalBugs = this.props.bugs.filter(
+        (bug) =>
+          bug.bugPriority === "Critical" && bug.bugDueDate && !bug.bugComplete
+      );
+      criticalBugs.sort((firstBug, secondBug) => {
+        return new Date(firstBug.bugDueDate) - new Date(secondBug.bugDueDate);
+      });
+      bugArray = bugArray.concat(criticalBugs);
+
+      // High
+      let highBugs = this.props.bugs.filter(
+        (bug) =>
+          bug.bugPriority === "High" && bug.bugDueDate && !bug.bugComplete
+      );
+      highBugs.sort((firstBug, secondBug) => {
+        return new Date(firstBug.bugDueDate) - new Date(secondBug.bugDueDate);
+      });
+      bugArray = bugArray.concat(highBugs);
+
+      // Medium
+      let mediumBugs = this.props.bugs.filter(
+        (bug) =>
+          bug.bugPriority === "Medium" && bug.bugDueDate && !bug.bugComplete
+      );
+      mediumBugs.sort((firstBug, secondBug) => {
+        return new Date(firstBug.bugDueDate) - new Date(secondBug.bugDueDate);
+      });
+      bugArray = bugArray.concat(mediumBugs);
+
+      // Low
+      let lowBugs = this.props.bugs.filter(
+        (bug) => bug.bugPriority === "Low" && bug.bugDueDate && !bug.bugComplete
+      );
+      lowBugs.sort((firstBug, secondBug) => {
+        return new Date(firstBug.bugDueDate) - new Date(secondBug.bugDueDate);
+      });
+      bugArray = bugArray.concat(lowBugs);
+
+      // Generate table elements and return first 10
+      return bugArray
+        .map((bug, index) => {
+          return (
+            <tr key={index}>
+              <td>
+                <Link
+                  to={`/projects/${this.getProjectIdCode(bug.project_id)}/${
+                    bug.bugId
+                  }`}
+                >
+                  {bug.bugId}
+                </Link>
+              </td>
+              <td>{bug.bugTitle}</td>
+              <td>{bug.bugPriority}</td>
+              <td>{bug.bugDueDate}</td>
+              <td>{bug.bugStatus}</td>
+              <td>{this.getProjectIdCode(bug.project_id)}</td>
+            </tr>
+          );
+        })
+        .slice(0, 10);
+    }
+  }
+
+  getProjectIdCode(id) {
+    return this.props.projects.filter((project) => project.id === id)[0]
+      .projectId;
   }
 }
 
