ee7f434b70c916f0237b94c71e5e67a846f68ff6
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-14 13:21:49 -0800

    Add Redux connection to projects page

--- cyberbug_frontend/src/containers/pages/projectsPage.js
+++ cyberbug_frontend/src/containers/pages/projectsPage.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import ProjectListDetailed from "../../components/dashboard/projectListDetailed";
 
 class ProjectsPage extends Component {
   render() {
     return (
       <div>
-        <ProjectListDetailed />
+        <ProjectListDetailed
+          projects={this.props.projects}
+          bugs={this.props.bugs}
+        />
       </div>
     );
   }
 }
 
-export default ProjectsPage;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.projects.allProjects,
+    bugs: state.bugs.allBugs,
+  };
+};
+
+export default connect(mapStateToProps)(ProjectsPage);
