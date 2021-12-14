01b4970e16942fcba6480202b686a7473cc7280c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-12-13 19:31:57 -0800

    Style 404 page

--- cyberbug_frontend/src/containers/pages/pageNotFound.js
+++ cyberbug_frontend/src/containers/pages/pageNotFound.js
@@ -1,15 +1,19 @@
 import React from "react";
+import PageBackground from "../../components/pageBackground";
 
 const PageNotFound = () => {
   return (
-    <div className="full-page centered-flex">
-      <div className="centered-flex-vert">
-        <img src="/cyberbugLogoQuick.png" alt="Cyberbug Logo" />
-        <h1>Woops, page not found!</h1>
-        <button onClick={() => window.history.back()}>
-          Go Back to Safety!
-        </button>
+    <div>
+      <div className="full-page centered-flex">
+        <div className="centered-flex-vert">
+          <img src="/cyberbugLogoQuick.png" alt="Cyberbug Logo" />
+          <h1>Whoops, page not found!</h1>
+          <button onClick={() => window.history.back()} id="page-back-btn">
+            Go Back to Safety!
+          </button>
+        </div>
       </div>
+      {/* <PageBackground /> */}
     </div>
   );
 };
